b344bbf2e2473201814b3311bbda731d3eb417ec
Aleksander Moloniewicz <a.moloniewicz@moja-gastronomia.com>
2016-06-13 16:14:23 +0100

    Set up gulp to concatenate scripts for development folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
 
@@ -10,6 +11,13 @@ gulp.task('coffee', function () {
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'));
   });
+
+gulp.task('scripts', function () {
+  gulp.src('components/scripts/**/*.js')
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js')
+        .on('error', gutil.log));
+});
 
 // gulp.task('log', function () {
 //   gutil.log('stuff happened', 'Really it did', gutil.colors.cyan(1));
