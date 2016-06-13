79982245721cb9fc6035a4a6b650f80a5d098e69
Aleksander Moloniewicz <a.moloniewicz@moja-gastronomia.com>
2016-06-13 15:56:24 +0100

    Set up coffescript processing in gulp
    -build first coffee script for builds
    -add git url to package.json

---
+++ components/scripts/tagline.js
@@ -0,0 +1,7 @@
+var fill;
+
+(fill = function(item) {
+  return $('.tagline').append("#item");
+})('Themost creative minds in Art');
+
+fill;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('testing g-util');
+var coffeeSources = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', function () {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'));
   });
+
+// gulp.task('log', function () {
+//   gutil.log('stuff happened', 'Really it did', gutil.colors.cyan(1));
+// });
