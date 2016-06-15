6458212a76d0faab776c50498476cd0212720eb5
Aleksander Moloniewicz <a.moloniewicz@moja-gastronomia.com>
2016-06-15 15:49:28 +0100

    Set up gulpfile environments
    development - default
    production - use with shell NODE_ENV=production gulp
    production compresses styles
    -and moves the root folder to build/production

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,37 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     compass = require('gulp-compass');
 
+// Separate declaration from assignment
+var env,
+    coffeeSources,
+    jsSources,
+    sassSources,
+    htmlSources,
+    jsonSources,
+    outputDir,
+    sassStyle;
+
+// for env assign the NODE environemtn and if it is not set up assign development
+var env = process.env.NODE_ENV || 'development';
+
+gulp.task('test', function () {
+  console.log(process.env.NODE_ENV);
+});
+
+// set up output depending on the environment variable that we have set up
+if (env === 'development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded'; //might need to change something in scss files fro the sass command to run and overwrite the previous css filr
+  } else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+  }
+
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/**/*.js'];
 var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/**/*.html'];
+var htmlSources = [outputDir + '**/*.html'];
+var jsonSources = [outputDir + 'js/**/*.json'];
 
 // process coffee scripts to javascript and store in components/scripts
 gulp.task('coffee', function () {
@@ -23,7 +50,7 @@ gulp.task('scripts', function () {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify()) //for loading jquery install npm jquery and in js file use $ = require('jquery'); without var for global
-    .pipe(gulp.dest('builds/development/js')
+    .pipe(gulp.dest(outputDir + 'js')
     .on('error', gutil.log))
     .pipe(connect.reload()); //finish the task by reloading
 });
@@ -32,10 +59,10 @@ gulp.task('sass', function () {
   gulp.src(sassSources)
     .pipe(compass({
       sass: 'components/sass',
-      images: 'builds/development/images',
-      style: 'expanded'
+      images: outputDir + 'images',
+      style: sassStyle
     }))
-    .pipe(gulp.dest( 'builds/development/css'))
+    .pipe(gulp.dest( outputDir + 'css'))
     .pipe(connect.reload()); //finish the task by reloading
 });
 
@@ -49,13 +76,13 @@ gulp.task('watch', function () {
   gulp.watch(jsSources, ['scripts']);
   gulp.watch(['components/sass/**/*.scss'], ['sass']);
   gulp.watch(htmlSources, ['html']);
-  gulp.watch('builds/development/js/**/*.json', ['json']);
+  gulp.watch(jsonSources, ['json']);
 });
 
 
 gulp.task('connect', function () {
   connect.server({
-    root: 'builds/development', //use http://localhost:8080 to view site
+    root: outputDir, //use http://localhost:8080 to view site
     livereload: true //install livereload chrome exetnsion
   });
   });
@@ -68,7 +95,7 @@ gulp.task('html', function () {
 
 //reload for json files
 gulp.task('json', function () {
-  gulp.src('builds/development/js/**/*.json')
+  gulp.src(jsonSources)
     .pipe(connect.reload());
 });
 
