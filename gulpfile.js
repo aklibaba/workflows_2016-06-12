7c888052b4b9521d6d0c6bee075562180f4909be
Aleksander Moloniewicz <a.moloniewicz@moja-gastronomia.com>
2016-06-15 14:23:26 +0100

    Set up static file watch and reload
    html task
    json task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ var gulp = require('gulp'),
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/**/*.js'];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/**/*.html'];
 
+// process coffee scripts to javascript and store in components/scripts
 gulp.task('coffee', function () {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
@@ -20,10 +22,10 @@ gulp.task('coffee', function () {
 gulp.task('scripts', function () {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
-    .pipe(browserify())
+    .pipe(browserify()) //for loading jquery install npm jquery and in js file use $ = require('jquery'); without var for global
     .pipe(gulp.dest('builds/development/js')
-        .on('error', gutil.log))
-    .pipe(connect.reload());
+    .on('error', gutil.log))
+    .pipe(connect.reload()); //finish the task by reloading
 });
 
 gulp.task('sass', function () {
@@ -34,26 +36,41 @@ gulp.task('sass', function () {
       style: 'expanded'
     }))
     .pipe(gulp.dest( 'builds/development/css'))
-    .pipe(connect.reload());
+    .pipe(connect.reload()); //finish the task by reloading
 });
 
 
-gulp.task('default', ['coffee', 'scripts', 'sass', 'connect', 'watch']);
+
+//run all tasks and then watch
+gulp.task('default', ['coffee', 'scripts', 'sass', 'connect', 'html', 'json', 'watch']);
 
 gulp.task('watch', function () {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['scripts']);
   gulp.watch(['components/sass/**/*.scss'], ['sass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/js/**/*.json', ['json']);
 });
 
 
 gulp.task('connect', function () {
   connect.server({
-    root: 'builds/development',
-    livereload: true
+    root: 'builds/development', //use http://localhost:8080 to view site
+    livereload: true //install livereload chrome exetnsion
   });
   });
 
+//reload for html files
+gulp.task('html', function () {
+  gulp.src(htmlSources)
+    .pipe(connect.reload());
+});
+
+//reload for json files
+gulp.task('json', function () {
+  gulp.src('builds/development/js/**/*.json')
+    .pipe(connect.reload());
+});
 
 // gulp.task('log', function () {
 //   gutil.log('stuff happened', 'Really it did', gutil.colors.cyan(1));
